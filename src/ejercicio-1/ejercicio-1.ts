8351421e33c34b1b7b0305f9ac9a268b5a94c360
Dana Choque <alu0101328348@ull.edu.es>
2022-03-20 19:52:26 +0000

    prueba

--- src/ejercicio-1/ejercicio-1.ts
+++ src/ejercicio-1/ejercicio-1.ts
@@ -1,1 +1,8 @@
 // PRUEBA1
+// https://www.iteramos.com/pregunta/10580/como-generar-getters-y-setters-en-visual-studio
+// https://david-estevez.gitbooks.io/the-git-the-bad-and-the-ugly/content/es/subir-y-bajar-cambios-a-un-repositorio-remoto.html
+// eslint-disable-next-line no-unused-vars
+class Ejercicio1 {
+  constructor() {
+  }
+}
