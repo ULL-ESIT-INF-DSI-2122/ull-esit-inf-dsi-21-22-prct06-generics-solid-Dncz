7afec5ae67b7383fca6f79f96cd59789cdb069bf
Dana Choque <alu0101328348@ull.edu.es>
2022-03-27 17:38:23 +0000

    test

--- test/ejercicioPE103.spec.ts
+++ test/ejercicioPE103.spec.ts
@@ -5,20 +5,31 @@ import {Complex} from '../src/ejercicioPE/complex';
 import {Rational} from '../src/ejercicioPE/rational';
 
 describe("ejercicio-PE", () => {
-  let coleccioncomplex = new Complex([1, 2, 3]);
-  // const myRationalCollection = new ArithmeticableCollection<Rational>([
-  //   new Rational(5, 7), new Rational(1, 6)]);
+  const complexCollection = new ArithmeticableCollection<Complex>([
+    new Complex(4, 5),
+    new Complex(1, 2),
+  ]);
 
+  const rationalCollection2 = new ArithmeticableCollection<Rational>([
+    new Rational(2, 11), new Rational(1, 3)]);
 
   it("Clase  Complex:", () => {
-    expect(coleccioncomplex).to.be.instanceOf(Complex);
-    expect(coleccioncomplex.getComplexCollection()).to.deep.equal([1, 2, 3]);
-    expect(coleccioncomplex.add(1, 2)).to.be.equal(3);
-    expect(coleccioncomplex.substract(2, 3)).to.be.equal(-1);
-    expect(coleccioncomplex.multiply(3, 2)).to.be.equal(6);
-    expect(coleccioncomplex.divide(6, 2)).to.be.equal(3);
+    expect(complexCollection.getArithmeticable(0)).to.be.instanceOf(Complex);
+    expect(complexCollection.getArithmeticable(1)).to.be.instanceOf(Complex);
+    expect(complexCollection.getNumberOfArithmeticables()).to.be.equal(2);
+    expect(complexCollection.getArithmeticable(0).add(complexCollection.getArithmeticable(1)).toString()).to.be.equal("5 + 7i");
+    expect(complexCollection.getArithmeticable(0).substract(complexCollection.getArithmeticable(1)).toString()).to.be.equal("3 + 3i");
+    expect(complexCollection.getArithmeticable(0).multiply(complexCollection.getArithmeticable(1)).toString()).to.be.equal("-6 + 13i");
+    expect(complexCollection.getArithmeticable(0).divide(complexCollection.getArithmeticable(1)).toString()).to.be.equal("-1.2 + 2.6i");
   });
   it("Clase Rational:", () => {
-    // expect().to.be.equal();
+    expect(rationalCollection2.getArithmeticable(0)).to.be.instanceOf(Rational);
+    expect(rationalCollection2.getArithmeticable(1)).to.be.instanceOf(Rational);
+    expect(rationalCollection2.getArithmeticable(0).add(rationalCollection2.getArithmeticable(1)).toString()).to.be.equal("17 / 33");
+    expect(rationalCollection2.getArithmeticable(0).substract(rationalCollection2.getArithmeticable(1)).toString()).to.be.equal("-5 / 33");
+    expect(rationalCollection2.getArithmeticable(0).multiply(rationalCollection2.getArithmeticable(1)).toString()).to.be.equal("2 / 33");
+    expect(rationalCollection2.getArithmeticable(0).divide(rationalCollection2.getArithmeticable(1)).toString()).to.be.equal("6 / 11");
+    rationalCollection2.addArithmeticable(new Rational(42, 51));
+    expect(rationalCollection2.getNumberOfArithmeticables()).to.be.equal(3);
   });
 });
